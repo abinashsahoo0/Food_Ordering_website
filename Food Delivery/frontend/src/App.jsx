a238a914166c3eb6bb045972067a7fb13088b379
abinashsahoo0 <sahooabinash801@gmail.com>
2024-07-03 15:24:26 +0530

    commit

---
+++ Food Delivery/frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import {Route,Routes} from 'react-router-dom'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Home from './pages/Home/Home'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path = '/order' element={<PlaceOrder/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
