66693fb365bf9712fc5256f7c9843e5c74afcfaa
abinashsahoo0 <sahooabinash801@gmail.com>
2024-07-04 16:27:16 +0530

    changes

--- Food Delivery/frontend/src/pages/Cart/Cart.jsx
+++ Food Delivery/frontend/src/pages/Cart/Cart.jsx
@@ -4,34 +4,38 @@ import { StoreContext } from '../../context/StoreContext'
 
 const Cart = () => {
 
-    const {cartItems,food_list,removeFromCart} = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
 
   return (
     <div className='cart'>
       <div className="cart-items">
         <div className="cart-items-title">
+
           <p>Items</p>
           <p>Title</p>
           <p>Price</p>
           <p>Quantity</p>
           <p>Total</p>
           <p>Remove</p>
+
         </div>
         <br />
         <hr />
-        {food_list.map((item,index)=>{
-          if(cartItems[item._id]>0)
-          {
-            return (
-              <div className='class-items-title cart-items-item'>
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (<div key={index}>
+              <div className='cart-items-title cart-items-item'>
                 <img src={item.image} alt="" />
                 <p>{item.name}</p>
                 <p>{item.price}</p>
                 <p>{cartItems[item._id]}</p>
-                <p>{item.price*cartItems[item._id]}</p>
+                <p>{item.price * cartItems[item._id]}</p>
                 <p>x</p>
+
+
               </div>
-            )
+              <hr />
+            </div>)
           }
         })}
       </div>
