e46fc5c5fa0b1c1779e2f3319d433d773abb3c84
Ratnakar03 <ratnakar.patra2003@gmail.com>
2024-07-14 19:44:09 +0530

    ^_^

--- Food Delivery/frontend/src/pages/MyOrders/MyOrders.jsx
+++ Food Delivery/frontend/src/pages/MyOrders/MyOrders.jsx
@@ -36,7 +36,7 @@ const MyOrders = () => {
                   }
                   
                 })}</p>
-                <p>{order.amount}.00</p>
+                <p>${order.amount}.00</p>
                 <p>Items: {order.items.length}</p>
                 <p><span>&#x25cf;</span> <b>{order.status}</b></p>
                 <button onClick={fetchOrders}>Track Order</button>
